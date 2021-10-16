ccb931a61b687287220a25a4d186ae9037743b9d
hata6502 <hata6502@users.noreply.github.com>
2021-10-16 17:03:48 +0000

    deploy: 30333ff69dd7e4a2a42dd345521516c0911072b7

---
+++ serviceWorker.js
@@ -0,0 +1,49 @@
+const cacheName = "v3";
+const requests = ["https://cdn.jsdelivr.net/npm/kanvas-dialog@latest", "."];
+
+// eslint-disable-next-line no-restricted-globals
+const serviceWorker = self;
+
+serviceWorker.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+
+      await Promise.all(
+        keys.map(async (key) => {
+          if (key === cacheName) {
+            return;
+          }
+
+          await caches.delete(key);
+        })
+      );
+    })()
+  );
+});
+
+serviceWorker.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cacheResponse = await caches.match(event.request);
+
+      if (cacheResponse) {
+        return cacheResponse;
+      }
+
+      const fetchResponse = await fetch(event.request);
+
+      return fetchResponse;
+    })()
+  );
+});
+
+serviceWorker.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+
+      return cache.addAll(requests);
+    })()
+  );
+});
